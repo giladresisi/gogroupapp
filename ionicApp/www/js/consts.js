c06fa6790ead0a541de094b1f6627637b98e310a
Gilad Resisi <giladresisi@gmail.com>
2017-04-04 03:33:53 +0300

    hebrew

--- ionicApp/www/js/consts.js
+++ ionicApp/www/js/consts.js
@@ -2,7 +2,7 @@
 
 app
 //.constant('BACKEND_URL', 'http://sampleEnv.gewji6ncrf.us-west-2.elasticbeanstalk.com/')
-//.constant('BACKEND_URL', 'http://gogroupapp.us-west-2.elasticbeanstalk.com/')
-.constant('BACKEND_URL', 'http://localhost:3000/')
+.constant('BACKEND_URL', 'http://gogroupapp.us-west-2.elasticbeanstalk.com/')
+//.constant('BACKEND_URL', 'http://localhost:3000/')
 .constant('FACEBOOK_CLIENT_ID', '1805278553072146')
 .constant('GOOGLE_CLIENT_ID', '1028051752871-htnbl98ak3ui6rl8lvt9bdker5efuihu.apps.googleusercontent.com');
